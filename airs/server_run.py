b64b28f4a788c6dd07e34d5fadf8886df97ead27
Patemole <77934579+Patemole@users.noreply.github.com>
2024-06-04 15:09:32 +1000

    Ajout de la fonction academic_advisor et création des endpoints

--- airs/server_run.py
+++ airs/server_run.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import server_cluster
 import server_files
+import server_academic_advisor
 
 def run_serveur_cluster():
     server_cluster.run()
@@ -10,12 +11,19 @@ def run_serveur_file():
     server_files.run()
 
 
+def run_serveur_chat():
+    server_academic_advisor.run()
+
+
 if __name__ == "__main__":
     p1 = multiprocessing.Process(target=run_serveur_cluster)
     p2 = multiprocessing.Process(target=run_serveur_file)
+    p3 = multiprocessing.Process(target=run_serveur_chat)
     
     p1.start()
     p2.start()
+    p3.start()
     
     p1.join()
     p2.join()
+    p3.join()
